365e3eedf12628e2e84a5fb6677e9c143652fa9e
Diana Pamela Sanchez Campos <anaid.cm31@gamil.com>
2024-07-26 18:46:29 -0600

    Configuración de webpack y estructura HTML

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js', //Punto de entrada de la aplicación
+    output: {
+        filename: 'bundle.js', //Nombre del archivo de salida
+        path: path.resolve(__dirname, 'dist'), //Carpeta de salida
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/, //Regex para identificar archivos css
+                use: ['style-loader', 'css-louder'], //Librerías louders que sirven para procesar archivos CSS
+            },
+            {
+                test: /\.js$/, //Regex sirve para identificar archivos JS
+                exclude: /node_modules/, //Excluir la carpeta node modules
+                use: {
+                    loader: 'babel-loader', //Loader para llevar JS moderno a JS antigui para que sea compatible con todos los navegadores
+                    options: {
+                        presets: ['@babel/preset-env'] 
+                    },
+                },
+            },
+        ],
+    },
+    devtool: 'source-map', //Herramienta para generar mapas de código para facilitar la depuración
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'), //Carpeta desde el cual el servidor tomará los archivos
+        compress: true, //Habilitar compresión gzip
+        port:9000, //Puerto del servidor de desarrollo
+    },
+};
